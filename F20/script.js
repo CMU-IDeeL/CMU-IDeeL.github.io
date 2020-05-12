4e8e0cde21de75ff681593d1e02ad2fa0427e503
Antioch Sanders <antiochsanders@gmail.com>
2020-05-12 14:39:53 -0400

    Make experimental website changes

---
+++ F20/script.js
@@ -0,0 +1,23 @@
+function showMenu() {
+    console.log("Menu Shown");
+    $('.mobile-menu').addClass('vis');
+    $('.menu-backdrop').addClass('vis');
+    //$('#mobile-menu, #menu-backdrop').css('display', 'block');
+}
+
+function hideMenu() {
+    $('.mobile-menu').removeClass('vis');
+    $('.menu-backdrop').removeClass('vis');
+}
+
+const estimatedHeaderHeight = 60;
+function toSection(target) {
+    var offset = $(target).offset();
+    var scrollto = offset.top - estimatedHeaderHeight;
+    $('html, body').animate({scrollTop:scrollto}, 100);
+}
+
+function mobileToSection(target) {
+    hideMenu();
+    toSection(target);
+}
